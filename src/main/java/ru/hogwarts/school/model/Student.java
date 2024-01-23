864cac3c7440b1ec0751a59a430ac8ef0918cb56
Westender <javaxwestender@gmail.com>
2024-01-24 00:21:34 +0300

    add default constructor

--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -16,7 +16,10 @@ public class Student {
     private String name;
     private int age;
 
-    public Student(long id, String name, int age) {
+    public Student() {
+    }
+
+    public Student(Long id, String name, int age) {
         this.id = id;
         this.name = name;
         this.age = age;
