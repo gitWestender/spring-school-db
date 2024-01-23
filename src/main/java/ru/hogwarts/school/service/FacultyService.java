00724a77c8375dd8030ad7ee9cc0016fedae8826
Westender <javaxwestender@gmail.com>
2024-01-24 00:21:54 +0300

    add CRUD

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,6 +1,9 @@
 package ru.hogwarts.school.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.FacultyRepository;
 
 @Service
@@ -12,5 +15,20 @@ public class FacultyService {
         this.facultyRepository = facultyRepository;
     }
 
+    public Faculty createFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public Object findFaclty(Long id) {
+        return facultyRepository.findById(id);
+    }
+
+    public Faculty editFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public void deleteFaculty(Long id) {
+        facultyRepository.deleteById(id);
+    }
 
 }
