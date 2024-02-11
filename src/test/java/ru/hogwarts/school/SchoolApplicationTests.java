3e44053a93c67a045d4761a9e44bedb00ce37765
gitWestender <javaxwestender@gmail.com>
2024-02-11 14:01:55 +0300

    Initial branch commit

--- src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
+++ src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
@@ -2,9 +2,13 @@ package ru.hogwarts.school;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+@SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SchoolApplicationTests {
+
+	@LocalServerPort
+	private int port;
 
 	@Test
 	void contextLoads() {
