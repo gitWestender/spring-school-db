d390b3ab95f3c9e9eb8649b1d474a66da1a0232a
gitWestender <javaxwestender@gmail.com>
2024-03-20 20:39:05 +0300

    Add FacultyController Mock Tests

--- src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
+++ src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
@@ -1,14 +1,8 @@
 package ru.hogwarts.school;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SchoolApplicationTests {
-
-	@LocalServerPort
-	private int port;
 
 	@Test
 	void contextLoads() {
