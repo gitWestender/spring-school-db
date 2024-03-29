f6661943e5323a34822ffdfae0491aea6d41c865
gitWestender <javaxwestender@gmail.com>
2024-03-29 18:54:25 +0300

    complete part 1

--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -2,6 +2,7 @@ package ru.hogwarts.school.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.hogwarts.school.model.Student;
 
 import java.util.Collection;
@@ -10,6 +11,18 @@ import java.util.List;
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Collection<Student> findStudentsByAgeBetween(Integer min, Integer max);
 
-    @Query (value = "DELETE FROM students WHERE id = (SELECT id FROM students ORDER BY id DESC LIMIT 1)", nativeQuery = true)
-    Collection<Student> deleteLastAdded();
+    @Query(
+            value = "select count(distinct (name)) from students",
+            nativeQuery = true)
+    Integer getNumbersOfAllStudents();
+
+    @Query(
+            value = "select avg(students.age) as age from students",
+            nativeQuery = true)
+    Integer getAvgAgeOfStudents();
+
+    @Query(
+            value = "select * from students order by id desc limit ?",
+            nativeQuery = true)
+    List<Student> getLastStudents(Integer count);
 }
