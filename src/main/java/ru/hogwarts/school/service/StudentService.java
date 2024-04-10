c7255e314c5007bcc21a8c8c901057e1a2f87965
gitWestender <javaxwestender@gmail.com>
2024-04-10 20:33:54 +0300

    add Logger

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,6 +1,8 @@
 package ru.hogwarts.school.service;
 
 import jakarta.persistence.criteria.CriteriaBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exceptions.FacultyNotFoundException;
@@ -16,6 +18,8 @@ import java.util.function.Supplier;
 @Service
 public class StudentService {
 
+    Logger logger = LoggerFactory.getLogger(StudentService.class);
+
     @Autowired
     private StudentRepository studentRepository;
 
@@ -24,6 +28,7 @@ public class StudentService {
     }
 
     public Student findStudent(Long id) {
+        logger.info("Was invoked method for GET student");
         Supplier<StudentNotFoundException> sup = new Supplier<StudentNotFoundException>() {
             @Override
             public StudentNotFoundException get() {
@@ -34,34 +39,42 @@ public class StudentService {
     }
 
     public Student editStudent(Student student) {
+        logger.info("Was invoked method to PATCH student");
         return studentRepository.save(student);
     }
 
     public void deleteStudent(Long id) {
+        logger.info("Was invoked method to DELETE student");
         studentRepository.deleteById(id);
     }
 
     public List<Student> getAllStudent() {
+        logger.info("Was invoked method to GET_ALL student");
         return studentRepository.findAll();
     }
 
     public Collection<Student> getStudentsByAgeBetween(Integer min, Integer max) {
+        logger.info("Was invoked method to GET_ALL_BETWEEN_AGE");
         return studentRepository.findStudentsByAgeBetween(min, max);
     }
 
     public Faculty getFacultyByStudent(Long id){
+        logger.info("Was invoked method to GET_STUDENT_FACULTY");
         return studentRepository.getReferenceById(id).getFaculty();
     }
 
     public Integer getSchoolStudentsCount() {
+        logger.info("Was invoked method to GET_NUMBER_OF_STUDENTS");
         return studentRepository.getNumbersOfAllStudents();
     }
 
     public Integer getAvgAgeOfAllStudents() {
+        logger.info("Was invoked method to GET_AVG_AGE");
         return studentRepository.getAvgAgeOfStudents();
     }
 
     public List<Student> getLastFiveStudents(Integer count) {
+        logger.info("Was invoked method to GET_LAST_STUDENTS");
         return studentRepository.getLastStudents(count);
     }
 }
